9d8a3d22574a4539ad63246e82e79a409121f6ef
elmedinhajdari1 <102145771+elmedinhajdari1@users.noreply.github.com>
2022-03-23 15:36:40 +0100

    Bug/Style Fix Login/Register

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php include('server.php');
- include('include/logout.php');?>
+include('include/logout.php'); ?>
 
 
 
@@ -10,39 +10,37 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
-        integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/utilities.css">
     <title>BuzzHosting | Internet Hosting</title>
 </head>
 
 <body>
-<?php include('include/header.php')  ?>
+    <?php include('include/header.php')  ?>
 
 
 
-<div class="content">
-  	<!-- notification message -->
-  	<?php if (isset($_SESSION['success'])) : ?>
-      <div class="error success" >
-      	<h3>
-          <?php 
-          	echo $_SESSION['success']; 
-          	unset($_SESSION['success']);
-          ?>
-      	</h3>
-      </div>
-  	<?php endif ?>
+    <div class="content">
+        <!-- notification message -->
+        <?php if (isset($_SESSION['success'])) : ?>
+            <div class="error success">
+                <h3>
+                    <?php
+                    echo $_SESSION['success'];
+                    unset($_SESSION['success']);
+                    ?>
+                </h3>
+            </div>
+        <?php endif ?>
 
 
-    <!-- logged in user information -->
-    <?php  if (isset($_SESSION['username'])) : ?>
-    	<p>Welcome <strong><?php echo $_SESSION['username']; ?></strong></p>
-    	<p> <a href="index.php?logout='1'" style="color: red;">logout</a> </p>
-    <?php endif ?>
-</div>
+        <!-- logged in user information -->
+        <?php if (isset($_SESSION['username'])) : ?>
+            <p>Welcome <strong><?php echo $_SESSION['username']; ?></strong></p>
+            <p> <a href="index.php?logout='1'" style="color: red;">logout</a> </p>
+        <?php endif ?>
+    </div>
 
     <!-- Showcase -->
     <section class="showcase">
@@ -55,20 +53,20 @@
             <div class="showcase-form card">
                 <h2>Register</h2>
                 <form method="post" action="index.php">
-                <?php include('errors.php'); ?>
+                    <?php include('errors.php'); ?>
                     <div class="form-control">
-                        <input  type="text" name="username" value="<?php echo $username; ?>" placeholder="Username" >
+                        <input type="text" name="username" value="<?php echo $username; ?>" placeholder="Username">
                     </div>
                     <div class="form-control">
-                        <input  type="text" name="email" value="<?php echo $email; ?>" placeholder="Email">
+                        <input type="text" name="email" value="<?php echo $email; ?>" placeholder="Email">
                     </div>
                     <div class="form-control">
-                        <input  type="password" name="password_1" placeholder="Password" >
+                        <input type="password" name="password_1" placeholder="Password">
                     </div>
                     <div class="form-control">
                         <input type="password" name="password_2" placeholder="Confirm Password">
                     </div>
-                    <input type="submit"  name="register" value="Register" class="btn btn-primary" >
+                    <input type="submit" name="register" value="Register" class="btn btn-primary">
                 </form>
             </div>
         </div>
@@ -76,113 +74,121 @@
 
     <!-- Stats -->
     <div class="between">
-    <section class="stats">
-        <div class="container">
-            <h3 class="stats-heading text-center my-1">
-                This is our most recent work,<br>with modern architecture and
-                scaling
-            </h3>
-            <div class="grid grid-3 text-center my-4">
-                <div>
-                    <i class="fas fa-user fa-3x"></i>
-                    <h3>1,413,941</h3>
-                    <p class="text-secondary ">Clients</p>
+        <section class="stats">
+            <div class="container">
+                <h3 class="stats-heading text-center my-1">
+                    This is our most recent work,<br>with modern architecture and
+                    scaling
+                </h3>
+                <div class="grid grid-3 text-center my-4">
+                    <div>
+                        <i class="fas fa-user fa-3x"></i>
+                        <h3>1,413,941</h3>
+                        <p class="text-secondary ">Clients</p>
+                    </div>
+                    <div>
+                        <i class="fas fa-city fa-3x"></i>
+                        <h3>92</h3>
+                        <p class="text-secondary ">Cities</p>
+                    </div>
+                    <div>
+                        <i class="fas fa-project-diagram fa-3x"></i>
+                        <h3>2,349,555</h3>
+                        <p class="text-secondary ">Projects</p>
+                    </div>
                 </div>
-                <div>
-                    <i class="fas fa-city fa-3x"></i>
-                    <h3>92</h3>
-                    <p class="text-secondary ">Cities</p>
+            </div>
+        </section>
+
+        <!--CLI-->
+        <section class="cli">
+            <div class="container grid text-center">
+
+                <div class="card hover1">
+                    <h3>TV Pack</h3>
+                    <h1 class="test1">8€/Month</h1>
+                    <ul>
+                        <li>300 TV Channels</li>
+                        <li class="underline"></li>
+                        <li>4K Resiver</li>
+                        <li class="underline"></li>
+                        <li>Best Channels</li>
+                        <li class="underline"></li>
+                        <li>Sportive Channels</li>
+                        <li class="underline"></li>
+                        <li>Professional Support</li>
+                    </ul>
+                    <a href="javascript:void(0);">
+                        <h1 class="order">Order now</h1>
+                    </a>
                 </div>
-                <div>
-                    <i class="fas fa-project-diagram fa-3x"></i>
-                    <h3>2,349,555</h3>
-                    <p class="text-secondary ">Projects</p>
+                <div class="card hover1">
+                    <h3>TV+Internet Pack</h3>
+                    <h1 class="test1">14€/Month</h1>
+                    <ul>
+                        <li>300 TV Channels</li>
+                        <li class="underline"></li>
+                        <li>4K Resiver</li>
+                        <li class="underline"></li>
+                        <li>100 Mbp/s DW</li>
+                        <li class="underline"></li>
+                        <li>40 Mbp/s DW</li>
+                        <li class="underline"></li>
+                        <li>Best Channels</li>
+                        <li class="underline"></li>
+                        <li>Sportive Channels</li>
+                        <li class="underline"></li>
+                        <li>Professional Support</li>
+                    </ul>
+                    <a href="javascript:void(0);">
+                        <h1 class="order">Order now</h1>
+                    </a>
+                </div>
+                <div class="card hover1">
+                    <h3>Cyber Power Pack</h3>
+                    <h1 class="test1">22€/Month</h1>
+                    <ul>
+
+
+                        <li>TV Pack</li>
+                        <li class="underline"></li>
+                        <li>4K Resiver</li>
+                        <li class="underline"></li>
+                        <li>500 Mbp/s DW</li>
+                        <li class="underline"></li>
+                        <li>100 Mbp/s UP</li>
+                        <li class="underline"></li>
+                        <li>Low Latency</li>
+                        <li class="underline"></li>
+                        <li>Netflix</li>
+                        <li class="underline"></li>
+                        <li>Professional Support</li>
+                    </ul>
+                    <a href="javascript:void(0);">
+                        <h1 class="order">Order now</h1>
+                    </a>
+                </div>
+                <div class="card hover1">
+                    <h3>Mega Power Pack</h3>
+                    <h1 class="test1">30€/Month</h1>
+                    <ul>
+                        <li>TV Pack</li>
+                        <li class="underline"></li>
+                        <li>1 Gbp/s DW</li>
+                        <li class="underline"></li>
+                        <li>250 Mbp/s UP</li>
+                        <li class="underline"></li>
+                        <li>Low Latency</li>
+                        <li class="underline"></li>
+                        <li>Free Netflix+Hulu</li>
+                        <li class="underline"></li>
+                        <li>Professional Support</li>
+                    </ul>
+                    <a href="javascript:void(0);">
+                        <h1 class="order">Order now</h1>
+                    </a>
                 </div>
             </div>
-        </div>
-    </section>
-
-    <!--CLI-->
-    <section class="cli">
-        <div class="container grid text-center">
-
-            <div class="card hover1">
-                <h3>TV Pack</h3>
-                <h1 class="test1">8€/Month</h1>
-                <ul>
-                    <li>300 TV Channels</li>
-                    <li class="underline"></li>
-                    <li>4K Resiver</li>
-                    <li class="underline"></li>
-                    <li>Best Channels</li>
-                    <li class="underline"></li>
-                    <li>Sportive Channels</li>
-                    <li class="underline"></li>
-                    <li>Professional Support</li>
-                </ul>
-                <a href="javascript:void(0);"><h1 class="order">Order now</h1></a>
-            </div>
-            <div class="card hover1">
-                <h3>TV+Internet Pack</h3>
-                <h1 class="test1">14€/Month</h1>
-                <ul>
-                    <li>300 TV Channels</li>
-                    <li class="underline"></li>
-                    <li>4K Resiver</li>
-                    <li class="underline"></li>
-                    <li>100 Mbp/s DW</li>
-                    <li class="underline"></li>
-                    <li>40 Mbp/s DW</li>
-                    <li class="underline"></li>
-                    <li>Best Channels</li>
-                    <li class="underline"></li>
-                    <li>Sportive Channels</li>
-                    <li class="underline"></li>
-                    <li>Professional Support</li>
-                </ul>
-                <a href="javascript:void(0);"><h1 class="order">Order now</h1></a>
-            </div>
-            <div class="card hover1">
-                <h3>Cyber Power Pack</h3>
-                <h1 class="test1">22€/Month</h1>
-                <ul>
-
-
-                    <li>TV Pack</li>
-                    <li class="underline"></li>
-                    <li>4K Resiver</li>
-                    <li class="underline"></li>
-                    <li>500 Mbp/s DW</li>
-                    <li class="underline"></li>
-                    <li>100 Mbp/s UP</li>
-                    <li class="underline"></li>
-                    <li>Low Latency</li>
-                    <li class="underline"></li>
-                    <li>Netflix</li>
-                    <li class="underline"></li>
-                    <li>Professional Support</li>
-                </ul>
-                <a href="javascript:void(0);"><h1 class="order">Order now</h1></a>
-            </div>
-            <div class="card hover1">
-                <h3>Mega Power Pack</h3>
-                <h1 class="test1">30€/Month</h1>
-                <ul>
-                    <li>TV Pack</li>
-                    <li class="underline"></li>
-                    <li>1 Gbp/s DW</li>
-                    <li class="underline"></li>
-                    <li>250 Mbp/s UP</li>
-                    <li class="underline"></li>
-                    <li>Low Latency</li>
-                    <li class="underline"></li>
-                    <li>Free Netflix+Hulu</li>
-                    <li class="underline"></li>
-                    <li>Professional Support</li>
-                </ul>
-                <a href="javascript:void(0);"><h1 class="order">Order now</h1></a>
-            </div>
-        </div>
     </div>
     </section>
 
@@ -208,4 +214,5 @@
 
     <script src="js/script.js"></script>
 </body>
+
 </html>
